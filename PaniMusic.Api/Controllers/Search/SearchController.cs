b52672394daa9d251659288b53deccc8a4226236
microsoft <microsoft@DESKTOP-9I4BSO0>
2021-04-15 11:19:20 +0430

    create search & visit controllers for api

---
+++ PaniMusic.Api/Controllers/Search/SearchController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PaniMusic.Services.ApplicationServices.Search.SearchAlbums;
+using PaniMusic.Services.ApplicationServices.Search.SearchMusicVideos;
+using PaniMusic.Services.ApplicationServices.Search.SearchTracks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaniMusic.Api.Controllers.Search
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+        private readonly ISearchTracks searchTracks;
+
+        private readonly ISearchAlbums searchAlbums;
+
+        private readonly ISearchMusicVideos searchMusicVideos;
+
+        public SearchController(ISearchTracks searchTracks
+            , ISearchAlbums searchAlbums
+            , ISearchMusicVideos searchMusicVideos)
+        {
+            this.searchTracks = searchTracks;
+
+            this.searchAlbums = searchAlbums;
+
+            this.searchMusicVideos = searchMusicVideos;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> SearchTrack([FromQuery] string trackName)
+        {
+            var getTrack = await searchTracks.SearchTrack(trackName);
+
+            if (getTrack == null)
+                return NotFound();
+
+            return Ok(getTrack);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> SearchAlbum([FromQuery] string albumName)
+        {
+            var getAlbum = await searchAlbums.SearchAlbum(albumName);
+
+            if (getAlbum == null)
+                return NotFound();
+
+            return Ok(getAlbum);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> SearchMusicVideo([FromQuery] string musicVideoName)
+        {
+            var getMusicVideo = await searchMusicVideos.SearchMusicVideo(musicVideoName);
+
+            if (getMusicVideo == null)
+                return NotFound();
+
+            return Ok(getMusicVideo);
+        }
+    }
+}
---
+++ PaniMusic.Api/Controllers/VisitStatistics/VisitStatisticsController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PaniMusic.Services.ApplicationServices.VisitStatistics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaniMusic.Api.Controllers.VisitStatistics
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class VisitStatisticsController : ControllerBase
+    {
+        private readonly IVisitStatistics visitStatistics;
+
+        public VisitStatisticsController(IVisitStatistics visitStatistics)
+        {
+            this.visitStatistics = visitStatistics;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> TrackVisitStatistics()
+        {
+            var getTrackVisits = await visitStatistics.TrackVisitStatistics();
+
+            return Ok(getTrackVisits);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AlbumVisitStatistics()
+        {
+            var getAlbumVisits = await visitStatistics.AlbumVisitStatistics();
+
+            return Ok(getAlbumVisits);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> MusicVideoVisitStatistics()
+        {
+            var getMusicVideoVisits = await visitStatistics.MusicVideoStatistics();
+
+            return Ok(getMusicVideoVisits);
+        }
+    }
+}
